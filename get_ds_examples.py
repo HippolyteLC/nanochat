3ac201dfeb346091f505b472a30527a49065c5c7
HippolyteLC <h.lecarreres1@gmail.com>
2026-09-23 12:14:59 +0200

    update

--- get_ds_examples.py
+++ get_ds_examples.py
@@ -9,11 +9,11 @@ def get_ds_examples(n_examples, ds_list):
             print(f"Example {i+1}\n")
             print(ds.get_example(i), "\n")
 
-if __name__ == "main":
-    n_examples = 3
-    ds_list = [
-        SmolTalk(split='test', stop=n_examples), 
-        GSM8K(split='test', stop=n_examples, subset='main'), 
-        MMLU(split='test', stop=n_examples, subset='all')
-    ]
-    get_ds_examples(n_examples,ds_list)
+#if __name__ == "main":
+n_examples = 3
+ds_list = [
+SmolTalk(split='test', stop=n_examples), 
+GSM8K(split='test', stop=n_examples, subset='main'), 
+MMLU(split='test', stop=n_examples, subset='all')
+]
+get_ds_examples(n_examples,ds_list)
